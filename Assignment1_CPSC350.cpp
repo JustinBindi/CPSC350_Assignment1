2dbbad78b74031777c4edf36a6d1c7b04b242367
Justin Bindi <bindi@chapman.edu>
2019-02-13 19:02:00 +0000

    adds reading from file line by line

--- Assignment1_CPSC350.cpp
+++ Assignment1_CPSC350.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <string> //dont know if needed
 using namespace std;
 
 int main(int argc, char * argv[])
 {
     string fileName = argv[1];
+    string line;
+
+    ifstream myFile (fileName);
+
+    while(!myFile.eof())
+    {
+      getline(myFile, line);
+      cout << line << endl;
+
+    }
+
+    myFile.close();
+
 
 
 
